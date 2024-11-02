a86777016665f084e22f0498f6f684f597a6789b
DeepikaSaravanan5 <deepika008@e.ntu.edu.sg>
2024-11-02 17:31:26 +0800

    readwrite for inventory??
    readwrite for inventory??

---
+++ hms/bin/com/hms/readwrite/readwritemedicine.java
@@ -0,0 +1,43 @@
+package com.hms.readwrite;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class readwritemedicine {
+   public static final String SEPARATOR = "|";
+
+  
+
+   public ArrayList <Medicine> Inventory(String filename) throws IOException {
+      File myFile = new File(filename);
+      Scanner sc = new Scanner(myFile);
+
+      ArrayList <Medicine> inventory=new ArrayList<>();
+
+      for(inventory = new ArrayList(); sc.hasNextLine(); inventory.add(medicine)) {
+       
+         String line = sc.nextLine();
+         StringTokenizer star = new StringTokenizer(line, "|");
+
+         String medicinename = star.nextToken().trim();
+         int initalstock = Integer.parseInt(star.nextToken().trim());
+         int lowstock = Integer.parseInt(star.nextToken().trim());
+         
+         Medicine medicine=new Medicine(medicinename, initialstock,lowstock);
+         
+      
+      }
+
+      sc.close();
+      return inventory;
+   }
+
+   public void saveUsers(String var1, List var2) throws IOException {
+      throw new Error("Unresolved compilation problem: \n\tThe method write(String, List) is undefined for the type TextDB\n");
+   }
+}
