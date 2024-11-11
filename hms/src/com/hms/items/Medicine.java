67b40d31f2ae4b1005bf8dd9daf230dc3f8b2ef5
Fay <faybeata.k@gmail.com>
2024-11-11 16:15:03 +0800

    req: merged pull request deepika to main

--- hms/src/com/hms/items/Medicine.java
+++ hms/src/com/hms/items/Medicine.java
@@ -5,53 +5,52 @@ public class Medicine {
 	private int med_id;
 	private int stock;
 	private int lowstock;
-	private double price;
-	
-	public Medicine(String medname, int med_id, int stock, int lowstock,double price)
-	{	
-		this.medname=medname;
-		this.med_id=med_id;
-		this.stock=stock;
-		this.lowstock=lowstock;
-		this.price=price;
+	private double price;  
+
+
+	public Medicine(String medname, int med_id, int stock, int lowstock, double price) {	
+		this.medname = medname;
+		this.med_id = med_id;
+		this.stock = stock;
+		this.lowstock = lowstock;
+		this.price = price;
 	}
-	
-	
-	
+
+
 	public String getMedname() {
 		return medname;
 	}
-	
+
 	
 	public int getMed_id() {
 		return med_id;
 	}
-	
+
+
 	public int getStock() {
 		return stock;
 	}
-	
+
 	public void setStock(int stock) {
-		this.stock=stock;
+		this.stock = stock;
 	}
+
+	
 	public int getLowstock() {
 		return lowstock;
 	}
 
+	public void setLowstock(int lowstock) {
+		this.lowstock = lowstock;
+	}
 
-
+	
 	public double getPrice() {
 		return price;
 	}
 
-
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	
-	
-	
-
 }
+
